54328ca8b3c7d0ae2b50a5035fdee725ed77577f
Andrey <priv1984@yandex.ru>
2022-05-09 08:30:58 +0700

    pop(), shift()

--- 01_types.js
+++ 01_types.js
@@ -1,9 +1,11 @@
-let users = [
-    {id: 1, name: "Вася"},
-    {id: 3, name: "Маша"},
-];
+let arr = [1, 2, 3, 4, 5];
 
-let user = users.find(item => item.id == 3);
-let userIndex = users.findIndex(item => item.id == 3);
 
-console.log(user.name);
+function arrPopShift() {
+    //const lengthArr = arr.length;
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr.shift());
+    }
+}
+
+arrPopShift();
