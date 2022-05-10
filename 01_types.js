53f4ed14c077fab70490648f2b9443018af11b10
Andrey <priv1984@yandex.ru>
2022-05-10 21:16:15 +0700

    Metod Array. tasks

--- 01_types.js
+++ 01_types.js
@@ -1,15 +1,11 @@
-function camelize(str) {
-    return str.split('-').map(
-        (b,e) => {
-            if(e>0){
-            b = b.split('');
-            b[0] = b[0].toUpperCase();
-            let c = b.join('')
-            //console.log(c)
-            return c}else{return b}
-        }
-    ).join('');
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+
+let copySorted = function (arr){
+  return [...arr].sort()
 }
 
-
-console.log(camelize('class-div-map'));
+let sorted = copySorted(arr);
+console.log(arr); // [3, 1]
+console.log(sorted); // [3, 1]
