7a517fee0aad17fc7c5f58a3892a5a5d1bbfa378
Andrey <priv1984@yandex.ru>
2022-05-11 22:42:02 +0700

    arr

--- 01_types.js
+++ 01_types.js
@@ -1,5 +1,6 @@
-console.time(5);
-console.log(5);
-console.timeLog(5);
+let arr = [2,4,5,7,23,5,456,546,35,23,5,6,34,423,34,1,234,35,6,78,79,0,89,865,5,6,578,76,54,36];
 
-console.timeEnd(5);
+
+
+let new_arr = arr.lastIndexOf(5);
+console.log(new_arr, arr.length)
