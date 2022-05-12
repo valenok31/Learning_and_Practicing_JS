6b1362cdb327fc5f6c84b5e3603b061b4e132504
Andrey <priv1984@yandex.ru>
2022-05-13 00:06:28 +0700

    object

--- 1_task.js
+++ 1_task.js
@@ -1,10 +1,15 @@
-// What the console will show?
-
+// #1 What the console will show?
 let arr = [1, 2, 3, 4, 5];
-
 function arrPopShift() {
     for (let i = 0; i < arr.length; i++) arr.shift()
     console.log(arr)
 }
-
 arrPopShift();
+
+// #2 What will be the result
+let func = (a = 2) => {
+    a ??= 3;
+    return a == 5;
+}
+let result = func(5)
+console.log(result)
--- copy_array.js
+++ copy_array.js
@@ -8,4 +8,5 @@ let arr_concat = arr.concat();
 let arr_filter = arr.filter(()=>true)
 let arr_JSON = JSON.parse(JSON.stringify(arr))
 
+
 console.log(arr_concat)
