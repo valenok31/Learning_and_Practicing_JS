4370a48d5597daf22cd37259b01009482834c11c
Andrey <priv1984@yandex.ru>
2022-05-14 00:20:02 +0700

    object

--- 01_types.js
+++ 01_types.js
@@ -1,16 +1,23 @@
-let calculator = {
-    read(a=1, b=1) {
-        this.a = a;
-        this.b = b;
-    },
-    sum() {
-        return this.a + this.b
-    },
-    mul() {
-        return this.a * this.b
-    }
+function Peopel(name = 'Anonim', age = 0) {
+    this.name = name;
+    this.age = age;
+    this.date = () => `${this.name} - ${2022 - this.age}`;
 };
 
-calculator.read();
-console.log(calculator.sum());
-console.log(calculator.mul());
+nika = new Peopel('Nika', 41);
+goga = new Peopel('Goga', 44);
+alis = new Peopel('Alisa', 8);
+andr = new Peopel('Andron', 38);
+an = new Peopel;
+
+console.log(alis.date())
+
+function eroba(){
+    let a = 4;
+     this.b = ()=> a;
+    return a
+}
+
+let r = new eroba();
+
+console.log(r.b())
