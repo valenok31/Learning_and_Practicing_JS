ef36a9db79e69b980a30c10a7072f558f77abbf4
Andrey <priv1984@yandex.ru>
2022-05-17 23:18:33 +0700

    encryptor

--- encryptor.js
+++ encryptor.js
@@ -1,6 +1,6 @@
-let str = "this is a secret message";
+let str = 'this is a secret message';
 let crypto = str.split(' ').map(a => parseInt(a, 36)).join(' ');
-console.log(crypto);
+console.log(crypto)
 
 let deCrypto = crypto.split(' ').map(a => (+a).toString(36)).join(' ');
 console.log(deCrypto);
