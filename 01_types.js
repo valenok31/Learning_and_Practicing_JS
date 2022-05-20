5c3ab4be639f52bd8552f73bdd560baa482d1f3e
Andrey <priv1984@yandex.ru>
2022-05-20 23:07:10 +0700

    Object.values(obj)) Object.keys(obj) Object.entries(obj) Object.fromEntries(obj)

--- 01_types.js
+++ 01_types.js
@@ -1,19 +1,15 @@
 let a = 'rebus';
-let arr = [1,2,3,4,5,7,7,8,88,76,5,4,3,]
+let arr = [1, 2, 3, 4, 5, 7, 7, 8, 88, 76, 5, 4, 3,]
 let obj = {
-    [arr]:arr,
-    'b':5,
-    c:34,
-    d:'let',
-    e:true,
-    f:new Map([
-        [8,true],
-        [true,'po'],
-        [NaN,'uoi']
-    ]),
-}
+    a: 4,
+    'b': 5,
+    c: 34,
+    d: 'let',
+    e: true,
+    f: 43,
+    }
 
+let arrArr = Object.entries(obj)
 
-console.log(Object.values(obj))
-console.log(Object.keys(obj))
-console.log(Object.entries(obj))
+console.log(arrArr[3]);
+console.log(Object.fromEntries(arrArr));
