7d2370c253f883b07b09fcb69de5c9bf68a354a4
Andrey <priv1984@yandex.ru>
2022-05-21 20:43:18 +0700

    Object.values(obj)) Object.keys(obj) Object.entries(obj) Object.fromEntries(obj)

--- 01_types.js
+++ 01_types.js
@@ -1,15 +1,5 @@
-let a = 'rebus';
-let arr = [1, 2, 3, 4, 5, 7, 7, 8, 88, 76, 5, 4, 3,]
-let obj = {
-    a: 4,
-    'b': 5,
-    c: 34,
-    d: 'let',
-    e: true,
-    f: 43,
-    }
+let inp = 'маршак самуил';
 
-let arrArr = Object.entries(obj)
+let [name, surname] = inp.split(' ')
 
-console.log(arrArr[3]);
-console.log(Object.fromEntries(arrArr));
+console.log(`${surname}, ${name}`)
