ab4e98b05237ad65d7e02e84052b38fadb1b0266
Andrey <priv1984@yandex.ru>
2022-05-25 07:17:33 +0700

    arguments

--- 01_types.js
+++ 01_types.js
@@ -1,10 +1,4 @@
+let arr = 'hellow'
 
-
-    let maxArgument = function(s,f,...arr){
-        console.log(s,f)
-        return arr.sort((a,b)=> b-a).shift();
-
-    }
-
-    console.log(maxArgument(3,543,5,4489))
+    console.log([...arr])
 
