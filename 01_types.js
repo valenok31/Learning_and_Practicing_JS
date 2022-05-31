e27742644b39933ca8ee322f6e9b83f5f65438ec
Andrey <priv1984@yandex.ru>
2022-05-31 23:15:10 +0700

    call/apply

--- 01_types.js
+++ 01_types.js
@@ -1,19 +1,3 @@
-function slowT(z) {
-    console.log(`z: ${z}`);
-    return z;
+function slow(){
+
 }
-
-function cachingDecorator(func) {
-    return function(xt) {
-        let result = func(xt);
-        return result;
-    };
-}
-//slow = function (xt){
-//         return slowT(xt);
-//     };
-
-
-slow = cachingDecorator(slowT);
-console.log(cachingDecorator(slowT)(5))
-console.log( slow(1) );
