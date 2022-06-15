1cc476870e61f0734577cee8e5e74fc2aae94318
Andrey <priv1984@yandex.ru>
2022-06-16 01:18:42 +0700

    get/set

--- 01_types.js
+++ 01_types.js
@@ -1,8 +1,14 @@
-const obj = {
-    propertyName: 'red'
-}
+let obj = {
+    get propName() {
+        console.log(5)
+        // геттер, срабатывает при чтении obj.propName
+    },
 
+    set propName(value) {
+        console.log(value)
+        // сеттер, срабатывает при записи obj.propName = value
+    }
+};
 
-let descriptor = Object.getOwnPropertyDescriptor(obj, 'propertyName');
-
-console.log(descriptor)
+obj.propName = 8;
+obj.propName;
