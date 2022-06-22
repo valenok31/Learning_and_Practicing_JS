e814045064e620617f7e370840bd0fa6213e876e
Andrey <priv1984@yandex.ru>
2022-06-22 22:58:42 +0700

    Promise

--- 01_types.js
+++ 01_types.js
@@ -1,19 +1,25 @@
-let promise = new Promise(function(resolve, reject) {
+let func = function(resolve, reject) {
+    console.log(typeof reject)
     setTimeout(function() {
         let isError = false;
 
         if (!isError) {
-            resolve([1, 2, 3, 4, 5]);   // данные промиса
+         resolve([1, 2, 3, 4, 5]);   // данные промиса
+
         } else {
             reject('error in promise'); // ваш текст ошибки
         }
     }, 500);
-});
+}
 
+
+let promise = new Promise(func);
+
+console.log(typeof promise.then)
 
 promise.then(
     function(result) {
-        console.log(result); // выведет результат промиса
+        console.log( result); // выведет результат промиса
     },
     function(error) {
         console.log(error);  // выведет текст ошибки
