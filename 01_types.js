ae3bf5dc48a89ed281f502e068b5881f79cddf3c
Andrey <priv1984@yandex.ru>
2022-06-28 00:06:00 +0700

    Promise.all/.race

--- 01_types.js
+++ 01_types.js
@@ -1,18 +1,41 @@
-const promise = new Promise((resolve, reject) => {
-
+const promise1 = new Promise((resolve, reject) => {
     const randomNumber = Math.random();
     setTimeout(() => {
-        if(randomNumber < .5) {
-            resolve(`Все прошло отлично! = ${randomNumber}`);
+        if(randomNumber < .8) {
+            resolve(`promise1 = ${randomNumber}`);
         } else {
-            reject(new Error(`Что-то пошло не так = ${randomNumber}`));
+            reject(new Error(`1 Что-то пошло не так = ${randomNumber}`));
+        }
+    }, randomNumber*5000);
+});
+const promise2 = new Promise((resolve, reject) => {
+    const randomNumber = Math.random();
+    setTimeout(() => {
+        if(randomNumber < .8) {
+            resolve(`promise2 = ${randomNumber}`);
+        } else {
+            reject(new Error(`2 Что-то пошло не так = ${randomNumber}`));
+        }
+    }, randomNumber*5000);
+});
+const promise3 = new Promise((resolve, reject) => {
+    const randomNumber = Math.random();
+    setTimeout(() => {
+        if(randomNumber < .8) {
+            resolve(`promise3 = ${randomNumber}`);
+        } else {
+            reject(new Error(`3 Что-то пошло не так = ${randomNumber}`));
         }
     }, randomNumber*5000);
 });
 
-promise.then(
+Promise.all([promise1,promise2,promise3])
+.then(
     (x)=> console.log(x)
 )
 .catch(
     (x)=> console.log(x)
+)
+.finally(
+    ()=> console.log('End')
 )
