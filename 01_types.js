9d3b3643ea1a64318954a899418054e5c224b755
Andrey <priv1984@yandex.ru>
2022-07-01 00:03:21 +0700

    Promise

--- 01_types.js
+++ 01_types.js
@@ -1,34 +1,29 @@
-const prom = new Promise(
-    (resolve, reject) => {
-        let rand = Math.random();
-        setTimeout(
-            () => {
-                if (rand < 0.2) {
-                    resolve(['resolve', rand])
-                } else {
-                    reject('reject');
-                }
-            }, rand*1000)
-    })
+const prom = new Promise((resolve, reject) => {
+    let rand = Math.random();
+    setTimeout(() => {
+        if (rand < 0.2) {
+            resolve('resolve')
+        } else {
+            reject('reject');
+        }
+    }, rand * 1000)
+})
 
 
 prom.then(
     (x) => {
-        console.log(x[1])
-        return x;
-    }
-)
-    .then(
-        (x) => {
-            console.log(x[0] + ' 2 then')
-        }
-    ).catch(
-    (x)=>{
         console.log(x)
+        throw new Error ("error!!!")
+
     }
 )
-.finally(
-    ()=> {
+    .catch(
+        error=> {
+            console.log(error)
+        }
+    )
+    .finally(
+    () => {
         console.log('End')
     }
 )
