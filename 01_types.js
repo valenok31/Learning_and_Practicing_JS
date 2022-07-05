e8dc939d02b4a430af036c0146103f856c88f7af
Andrey <priv1984@yandex.ru>
2022-07-06 00:12:52 +0700

    Promise, async / await

--- 01_types.js
+++ 01_types.js
@@ -1,14 +1,38 @@
-const myPromise = () => Promise.resolve('вопросик решен')
+const prom1 = new Promise((resolve, reject) => {
+    let rand = Math.random();
+    setTimeout(() => {
+        if (rand < 0.8) {
+            resolve('1 = ' + rand);
+        } else {
+            reject('Err')
+        }
+    }, rand * 1000)
+})
+const prom2 = new Promise((resolve, reject) => {
+    let rand = Math.random();
+    setTimeout(() => {
+        if (rand < 0.8) {
+            resolve('2 = ' + rand);
+        } else {
+            reject('Err')
+        }
+    }, rand * 1000)
+})
+const prom3 = new Promise((resolve, reject) => {
+    let rand = Math.random();
+    setTimeout(() => {
+        if (rand < 0.8) {
+            resolve('3 = ' + rand);
+        } else {
+            reject('Err')
+        }
+    }, rand * 1000)
+})
 
-function firstFunction() {
-    myPromise().then(res => console.log(res))
-    console.log('1')
-}
 
-async function secondFunction() {
-    console.log(await myPromise())
-    console.log('2')
-}
-
-firstFunction()
-secondFunction()
+Promise.all([prom3, prom1, prom2]).then(
+    x => console.log(x)
+)
+    .catch(
+        x => console.log(x)
+    )
