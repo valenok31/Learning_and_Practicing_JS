edd0d72f65e148a1f6dfc557719916605a7cbfcc
Andrey <priv1984@yandex.ru>
2022-07-08 12:14:26 +0700

    Promise, async / await

--- 01_types.js
+++ 01_types.js
@@ -1,13 +1,19 @@
-const promi = new Promise ((resolve, reject)=> {
-        let rand = Math.random();
-        if (rand < 0.5) {
-            resolve('Ok');
+const prom = new Promise((resolve, reject) => {
+    let rand = Math.trunc(Math.random() * 10) / 10;
+    setTimeout(
+        () => {
+            if (rand < 0.51) {
+                resolve([`Ok, rand = ${rand}`, rand]);
 
-        } else {
-            reject('Err')
-        }
-    }
- );
+            } else {
+                reject(`Err, rand = ${rand}`)
+            }
+        }, rand * 1000)
+});
 
-promi.then(x => console.log(x))
-    .catch(x=> console.log(x))
+prom.then(x => {
+    console.log(x[0])
+    return x[1]*2;
+})
+    .then(x => console.log(x))
+    .catch(x => console.log(x))
