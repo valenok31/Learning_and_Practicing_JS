deaee66f267c3c71b6f61d071c48938d0480ac3b
Andrey <priv1984@yandex.ru>
2022-07-19 00:37:40 +0700

    Promise, async / await

--- 01_types.js
+++ 01_types.js
@@ -3,11 +3,13 @@ async function f() {
     let promise = new Promise((resolve, reject) => {
         setTimeout(() => resolve("готово!"), 1000)
     });
-
+    console.log('start')
     let result = await promise; // будет ждать, пока промис не выполнится (*)
-
     console.log(result); // "готово!"
+
 }
 
+console.log('final')
 f();
 
+console.log('End')
