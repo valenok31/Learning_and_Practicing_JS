8c662225ad0a88bd247c766e9832a0e5b1e51be6
Andrey <priv1984@yandex.ru>
2022-07-29 00:40:40 +0700

    sum

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-for (let i = 1; i < 1; i++) {
-    let div = document.createElement('div');
-    div.className = "kletka";
-    div.id = 'sd' + i;
-    div.onclick = eventClickTdBox;
-    div.innerHTML = i;
-    wrapper.append(div);
-}
 let arr = [['X',5, 1, 3, 4, 6, 9, 7, 8],[7,'X', 9, 6, 1, 3, 2, 5, 8],[2,8,'X',1, 5, 9, 4, 6, 3],
     [5,8,9,'X',7, 1, 4, 3, 6],[2, 7, 9, 1,'X',4, 5, 8, 3],[9, 2, 3, 7, 4,'X',6,5,8],
     [9, 7, 8, 6, 2, 5,'X',4,3],[4, 8, 7, 1, 6, 2, 3,'X',9],[1, 5, 7, 3, 2, 8, 4, 6,'X']];
