c8ab85d0098a4229d977773145bf18aae9b9778d
Andrey <priv1984@yandex.ru>
2022-08-08 00:19:33 +0700

    onclick="event.stopPropagation()"

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let arr = [
-    [16, '<b>55</b>', 88, 14, 27, 35, 87, 96, 23], [76, 70, 32, 89, 15, 95, 29, 59, 97], [49, 10, 93, 67, 80, 62, 54, 12, 73],
+    [16, '55', 88, 14, 27, 35, 87, 96, 23], [76, 70, 32, 89, 15, 95, 29, 59, 97], [49, 10, 93, 67, 80, 62, 54, 12, 73],
     [43, 85, 58, 48, 68, 46, 74, 72, 22], [53, 60, 83, 42, 51, 91, 81, 37, 57], [56, 44, 77, 64, 66, 38, 52, 13, 40],
     [71, 11, 78, 34, 28, 39, 36, 21, 31], [90, 47, 94, 30, 99, 20, 86, 41, 63], [19, 61, 26, 33, 98, 82, 50, 69, 24]];
 let arrSum = [];
@@ -10,20 +10,20 @@ let divs = wrapper.getElementsByTagName('div');
 
 
 const clickedSquare = function (event) {
-    let idEvent = document.getElementById(this.id);
+    let idEvent = document.getElementById(event.target.id);
     if (idEvent.innerHTML != 'X') {
         idEvent.style.backgroundColor = 'red';
 
-        sum += +this.innerHTML;
-        sum2.push(+this.innerHTML);
+        sum += +event.target.innerHTML;
+        sum2.push(+event.target.innerHTML);
 
         let z = sum2.reduce((a, b) => {
             return a += b;
         })
         document.getElementById('wrapper__score').innerHTML = z;
         for (let divK of divs) {
-            document.getElementById(divK.id).innerText = arr[this.id][divK.id];
-            arr[divK.id][this.id] = 'X';
+            document.getElementById(divK.id).innerHTML = arr[event.target.id][divK.id];
+            arr[divK.id][event.target.id] = 'X';
         }
         idEvent.innerHTML = 'X';
         if (sum2.length == 9) {
@@ -34,9 +34,12 @@ const clickedSquare = function (event) {
     }
 }
 
-for (let divK of divs) {
+/*for (let divK of divs) {
     divK.onclick = clickedSquare;
-}
+}*/
+
+wrapper.onclick = clickedSquare;
+
 
 const resetClick = function () {
     for (let divK of divs) {
