c7f4820828a5237edbff1ba5db6d5e9d77cba343
Andrey <priv1984@yandex.ru>
2022-08-13 22:35:24 +0700

    new project: typing text

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 let taskText = "Advertisers study how people learn so that they can 'teach' them to respond to their advertising. They want us to be interested, to try something, and then to do it again. These are the elements of learning: interest, experience and repetition. If an advert can achieve this, it is successful. If an advert works well, the same technique can be used to advertise different things. So, for example, in winter if the weather is cold and you see a family having a warming cup of tea and feeling cosy, you may be interested and note the name of the tea ... Here the same technique is being used as with the cool, refreshing drink.";
 let rightRow = document.getElementById('rightRow');
-let leftRow = document.getElementById('leftRow');
+let leftRowBox = document.getElementById('leftRowBox');
 let boxErrors = document.getElementById('boxErrors');
 rightRow.innerHTML = taskText;
 let taskTextRight = rightRow.innerHTML;
 taskTextRight=taskTextRight.split('');
 let arr = [];
 let numberErrors=0;
+let numberLetters=0;
+
 
 
 const deleteRight = function (event) {
-    if(event.key===taskTextRight[0]){
+    //if(event.key===taskTextRight[0]){
+    if(true){
+        numberLetters +=1;
+        calculationError(numberErrors, numberLetters);
    arr.push(taskTextRight.shift());
-   if(arr.length>30){
+   if(arr.length>100){
        arr.shift();
-       leftRow.innerHTML = arr.join('');
+       leftRowBox.innerHTML = arr.join('');
    }else{
-       leftRow.innerHTML = arr.join('');
+       leftRowBox.innerHTML = arr.join('');
    }
     rightRow.innerHTML = taskTextRight.join('');
     }else{
@@ -25,12 +30,16 @@ const deleteRight = function (event) {
             setTimeout(()=>{
                 document.body.style.backgroundColor='';
                 numberErrors+=1;
-                boxErrors.innerHTML = numberErrors;
+                calculationError(numberErrors, numberLetters);
                 },50
             )
            // alert(event.key)
         }
     }
+}
+
+function calculationError (numberErrors, numberLetters){
+    boxErrors.innerHTML = Math.round((100-numberErrors*100/(numberLetters+1))*100)/100;
 }
 
 document.addEventListener("keydown", deleteRight)
