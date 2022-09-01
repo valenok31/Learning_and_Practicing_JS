7f5c7bdc4cf479895255afec8f1817012ca09621
Andrey <priv1984@yandex.ru>
2022-09-02 00:05:28 +0700

    target / relatedTarget

--- CellMovement/script.js
+++ CellMovement/script.js
@@ -2,7 +2,7 @@ let fieldPlaying = document.getElementById('fieldPlaying');
 let cellSelect = fieldPlaying.querySelectorAll('div');
 for (let elem of cellSelect) {
     elem.addEventListener('click', selectIdBox);
-    event.target.addEventListener('mouseover', fer);
+    elem.addEventListener('mouseover', fer);
 }
 
 
