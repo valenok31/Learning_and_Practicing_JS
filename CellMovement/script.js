1fce41f9108f94369b79722b614b79afee5e1a3e
Andrey <priv1984@yandex.ru>
2022-09-01 21:33:35 +0700

    target / relatedTarget

--- CellMovement/script.js
+++ CellMovement/script.js
@@ -1,18 +1,24 @@
 let fieldPlaying = document.getElementById('fieldPlaying');
 let cellSelect = fieldPlaying.querySelectorAll('div');
 for (let elem of cellSelect) {
-    elem.addEventListener('mouseover', selectIdBox);
+    elem.addEventListener('click', selectIdBox);
+    event.target.addEventListener('mouseover', fer);
 }
 
 
 function selectIdBox(event) {
-    if (event.relatedTarget.className === 'cell') {
+    event.target.dataset.about = 'true';
+
+
+}
+
+function fer(event) {
+    if (event.relatedTarget.className === 'cell' && event.target.dataset.about === 'true') {
         console.log(event.relatedTarget)
         let a = event.target.innerHTML;
-        let b = event.relatedTarget.innerHTML;
         event.target.innerHTML = event.relatedTarget.innerHTML
         event.relatedTarget.innerHTML = a;
-        //[a, b] = [b, a];
+
     }
     event.target.style.backgroundColor = 'red';
     event.relatedTarget.style.backgroundColor = '';
