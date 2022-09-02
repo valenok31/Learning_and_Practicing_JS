292004dfde86bc791d853eb19799b932717cfdb6
Andrey <priv1984@yandex.ru>
2022-09-02 21:55:01 +0700

    target / relatedTarget

--- CellMovement/script.js
+++ CellMovement/script.js
@@ -1,26 +1,24 @@
 let fieldPlaying = document.getElementById('fieldPlaying');
 let cellSelect = fieldPlaying.querySelectorAll('div');
+let unblocked = false;
 for (let elem of cellSelect) {
     elem.addEventListener('click', selectIdBox);
     elem.addEventListener('mouseover', fer);
 }
 
-
 function selectIdBox(event) {
-    event.target.dataset.about = 'true';
-
-
+    unblocked = !unblocked;
+    unblocked ? event.target.style.backgroundColor = 'red' : event.target.style.backgroundColor = ''
+    //event.target.style.backgroundColor = 'red';
 }
 
 function fer(event) {
-    if (event.relatedTarget.className === 'cell' && event.target.dataset.about === 'true') {
-        console.log(event.relatedTarget)
+    console.log(unblocked)
+    if (event.relatedTarget.className === 'cell' && unblocked) {
         let a = event.target.innerHTML;
         event.target.innerHTML = event.relatedTarget.innerHTML
         event.relatedTarget.innerHTML = a;
-
+        event.target.style.backgroundColor = 'red';
+        event.relatedTarget.style.backgroundColor = '';
     }
-    event.target.style.backgroundColor = 'red';
-    event.relatedTarget.style.backgroundColor = '';
-    //
 }
