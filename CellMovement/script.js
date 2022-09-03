939c3b4e061586868a58be45e36aa934f876f75b
Andrey <priv1984@yandex.ru>
2022-09-04 01:01:24 +0700

    color

--- CellMovement/script.js
+++ CellMovement/script.js
@@ -1,21 +1,24 @@
 let fieldPlaying = document.getElementById('fieldPlaying');
 window.addEventListener('scroll', selectIdBox);
+let colorDiv1 = `#${generatorColor()}`;
+let colorDiv2 = `#${generatorColor()}`;
 
-function generatorColor (){
-
-    let RR = Math.round(Math.random()*239+16);
-    let GG = Math.round(Math.random()*239+16);
-    let BB = Math.round(Math.random()*239+16);
-    let randomColor = RR.toString(16) + '' + GG.toString(16) + '' + BB.toString(16);
-    return randomColor;
+function generatorColor() {
+    let RR = Math.round(Math.random() * 239 + 16);
+     //RR = 250
+    let GG = Math.round(Math.random() * 239 + 16);
+    let BB = Math.round(Math.random() * 239 + 16);
+    return RR.toString(16) + '' + GG.toString(16) + '' + BB.toString(16);
 }
 
 function addDiv(i) {
-    let colorDiv = `#${generatorColor()}`;
+    colorDiv2 = colorDiv1;
+    colorDiv1 = `#${generatorColor()}`;
     let div = document.createElement('div');
     div.className = "cell";
-    div.innerHTML = `<strong>${colorDiv}</strong>`;
-    div.style.backgroundColor = colorDiv;
+    div.innerHTML = `<strong>${colorDiv1}</strong>`;
+    //div.style.backgroundColor = colorDiv;
+    div.style.background = "linear-gradient(to top, " + colorDiv1 + ", " + colorDiv2 + ")";
     fieldPlaying.append(div);
 }
 
@@ -29,15 +32,11 @@ function selectIdBox(event) {
         document.body.offsetHeight, document.documentElement.offsetHeight,
         document.body.clientHeight, document.documentElement.clientHeight
     );
-
     let clientHeightDDE = document.documentElement.clientHeight;
     let pageYOffset = window.pageYOffset;
-    //console.log(pageYOffset+clientHeightDDE + '/' + scrollHeight)
-    console.log((pageYOffset + clientHeightDDE) > (scrollHeight - 100))
-    if ((pageYOffset + clientHeightDDE) > (scrollHeight - 100)) {
 
+    if ((pageYOffset + clientHeightDDE) > (scrollHeight - 100)) {
         addDiv();
     }
-
 }
 
