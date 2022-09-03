a4d0294156601cf322ef26e8f7e1a8fcfbd574be
Andrey <priv1984@yandex.ru>
2022-09-03 23:32:52 +0700

    scroll

--- CellMovement/script.js
+++ CellMovement/script.js
@@ -1,26 +1,42 @@
 let fieldPlaying = document.getElementById('fieldPlaying');
-let cell = document.getElementById('0');
-let cellSelect = fieldPlaying.querySelectorAll('div');
+window.addEventListener('scroll', selectIdBox);
 
-window.addEventListener('mousemove', selectIdBox);
+function generatorColor (){
 
+    let RR = Math.round(Math.random()*239+16);
+    let GG = Math.round(Math.random()*239+16);
+    let BB = Math.round(Math.random()*239+16);
+    let randomColor = RR.toString(16) + '' + GG.toString(16) + '' + BB.toString(16);
+    return randomColor;
+}
+
+function addDiv(i) {
+    let colorDiv = `#${generatorColor()}`;
+    let div = document.createElement('div');
+    div.className = "cell";
+    div.innerHTML = `<strong>${colorDiv}</strong>`;
+    div.style.backgroundColor = colorDiv;
+    fieldPlaying.append(div);
+}
+
+for (let i = 0; i < 10; i++) {
+    addDiv(i)
+}
 
 function selectIdBox(event) {
+    let scrollHeight = Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+    );
 
-    cell.innerHTML = event.offsetX + '/' + event.offsetY
-
-    let scrollHeight = document.documentElement.scrollHeight;
     let clientHeightDDE = document.documentElement.clientHeight;
     let pageYOffset = window.pageYOffset;
-    let clientHeightDB = document.body.clientHeight;
-    console.log(pageYOffset + '/' + clientHeightDDE + '/' + clientHeightDB + '/' + scrollHeight)
+    //console.log(pageYOffset+clientHeightDDE + '/' + scrollHeight)
+    console.log((pageYOffset + clientHeightDDE) > (scrollHeight - 100))
+    if ((pageYOffset + clientHeightDDE) > (scrollHeight - 100)) {
 
-    if(window.pageYOffset + document.documentElement.clientHeight === document.body.clientHeight){
-        let div = document.createElement('div');
-        div.className = "cell";
-        div.innerHTML = "<strong>Всем привет!</strong> Вы прочитали важное сообщение.";
-
-        fieldPlaying.append(div);
+        addDiv();
     }
 
 }
