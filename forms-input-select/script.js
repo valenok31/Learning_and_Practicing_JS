35dc75c17fb4a8d91e1bfe7416d8503ae29e04e1
Andrey <priv1984@yandex.ru>
2022-09-06 19:40:20 +0700

    focus, blur

--- forms-input-select/script.js
+++ forms-input-select/script.js
@@ -1,12 +1,17 @@
-let form=document.forms.my;
+let input = document.getElementById('input');
+let error = document.getElementById('error');
 
-let select = form[0];
+input.onblur = function () {
+    if (!input.value.includes('@')) {
+        error.classList.add('invalid');
+        error.innerHTML = 'error. What is @?'
+        input.focus();
+    }
+}
 
-// все три строки делают одно и то же
-select.options[0].selected = true;
-//select.selectedIndex = 2;
-//select.value = 'banana';
-
-
-    console.log(Array.from(select.options));
-
+input.onfocus = function () {
+    if (error.classList.contains('invalid')) {
+        error.classList.remove('invalid');
+        error.innerHTML = '';
+    }
+}
