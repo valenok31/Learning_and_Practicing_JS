24dcbf1ddd748b06adf39f3e73bebdd1ca9a0e8d
Andrey <priv1984@yandex.ru>
2022-09-09 00:11:46 +0700

    RecAreaID - media

--- CellMovement/scriptRecareas.js
+++ CellMovement/scriptRecareas.js
@@ -8,9 +8,52 @@ window.addEventListener('scroll', selectIdBox);
 let colorDiv1 = `#${generatorColor()}`;
 let colorDiv2 = `#${generatorColor()}`;
 let infoTest;
-let infoTest2;
+let offsetS;
 let n = 0;
+let stopScroll = true;
 let queryTag = '';
+const instance = axios.create({
+    baseURL: 'https://ridb.recreation.gov/api/v1/',
+});
+
+werwer(queryTag, offsetS);
+
+function werwer(queryTag1, offset = 0) {
+    instance.get(`recareas?query=${queryTag1}&limit=20&offset=${offset}&apikey=53351234-6c6c-4392-a4b8-d38d53df1462`)
+        .then(function (response) {
+            inputAllData.innerHTML = response.data.METADATA.RESULTS.TOTAL_COUNT
+            infoTest = response.data.RECDATA;
+            for (let num of infoTest) {
+                instance.get(`/recareas/${num.RecAreaID}/media?apikey=53351234-6c6c-4392-a4b8-d38d53df1462`)
+                    .then(function (response) {
+                        stopScroll = true;
+                        addDiv(num, response.data.RECDATA[0].URL)
+                    });
+            }
+        });
+}
+
+function addDiv(infTes, werety) {
+    colorDiv2 = colorDiv1;
+    colorDiv1 = `#${generatorColor()}`;
+    let div = document.createElement('div');
+    div.className = "cell";
+    div.id = infTes.RecAreaID;
+    div.style.background = `url(${werety}) no-repeat center/cover`;
+    div.innerHTML = `<p><strong>${infTes.RecAreaName}</strong></p>`;
+    infTes.Keywords === '' ? div.title = 'none' : div.title = infTes.Keywords;
+    div.style.backgroundColor = colorDiv1;
+    fieldPlaying.append(div);
+}
+
+function photoSearch(photoInfo) {
+    instance.get(`/recareas/${photoInfo}/media?apikey=53351234-6c6c-4392-a4b8-d38d53df1462`)
+        .then(function (response) {
+            let infoTest2 = response.data.RECDATA;
+            return `${infoTest2[0].URL}`;
+        });
+}
+
 
 function searchR() {
     queryTag = keyword.value;
@@ -22,78 +65,13 @@ function searchR() {
 
 function generatorColor() {
     let RR = Math.round(Math.random() * 239 + 16);
-    //RR = 250
     let GG = Math.round(Math.random() * 39 + 216);
     let BB = Math.round(Math.random() * 39 + 216);
     return RR.toString(16) + '' + GG.toString(16) + '' + BB.toString(16);
 }
 
-werwer(queryTag);
 
-
-function werwer(queryTag) {// Make a request for a user with a given ID
-    const instance = axios.create({
-        baseURL: 'https://ridb.recreation.gov/api/v1/',
-    });
-    instance.get(`recareas?query=${queryTag}&limit=50&offset=0&apikey=53351234-6c6c-4392-a4b8-d38d53df1462`)
-        .then(function (response) {
-            inputAllData.innerHTML = response.data.METADATA.RESULTS.TOTAL_COUNT
-            infoTest = response.data.RECDATA;
-            addDiv();
-        });
-
-
-    console.log(infoTest)
-/*    let iMin = Math.min(infoTest.length, 10);
-    for (let i = 0; i < iMin; i++) {
-        addDiv()
-    }*/
-
-}
-
-function photoSearch(photoInfo) {
-    const instance2 = axios.create({
-        baseURL: 'https://ridb.recreation.gov/api/v1/',
-    });
-    instance2.get(`/recareas/${photoInfo}/media?apikey=53351234-6c6c-4392-a4b8-d38d53df1462`)
-        .then(function (response) {
-            infoTest2 = response.data.RECDATA;
-
-            // return `${infoTest2[0].URL}`;
-
-        });
-    //return `https://cdn.recreation.gov/public/images/68615.jpg`;
-    return `${infoTest2[0].URL}`;
-    console.log(infoTest2[0].URL)
-    //return `123456`;
-}
-
-
-function enumeration() {
-    return n++;
-}
-
-
-async function addDiv() {
-    let idN = enumeration();
-    let infTes = await infoTest[idN];
-    colorDiv2 = colorDiv1;
-    colorDiv1 = `#${generatorColor()}`;
-    let div = document.createElement('div');
-    div.className = "cell";
-    div.id = idN;
-   let werety = photoSearch(infTes.RecAreaID);
-
-    console.log(infTes)
-    div.style.background = `url(${werety}) no-repeat center/cover`;
-    div.innerHTML = `<p><strong>${infTes.RecAreaName}</strong></p>`;
-    infTes.Keywords === '' ? div.title = 'none' : div.title = infTes.Keywords
-    div.style.backgroundColor = colorDiv1;
-    fieldPlaying.append(div);
-}
-
-
-function selectIdBox(event) {
+function selectIdBox() {
     let scrollHeight = Math.max(
         document.body.scrollHeight, document.documentElement.scrollHeight,
         document.body.offsetHeight, document.documentElement.offsetHeight,
@@ -102,8 +80,10 @@ function selectIdBox(event) {
     let clientHeightDDE = document.documentElement.clientHeight;
     let pageYOffset = window.pageYOffset;
 
-    if ((pageYOffset + clientHeightDDE) > (scrollHeight - 300)) {
-        addDiv();
+    if ((pageYOffset + clientHeightDDE) > (scrollHeight - 300) && stopScroll) {
+        n = n + 20
+        stopScroll = false;
+        werwer(queryTag, n);
     }
 }
 
