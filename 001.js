73ca0acfa660ac4432f0c88aee30b821951dc28c
Andrey <priv1984@yandex.ru>
2023-06-11 22:35:01 +0700

    IP Geo Location

--- 001.js
+++ 001.js
@@ -1,19 +1,7 @@
-ret();
-
-async function ret(){const url = 'https://ip-geo-location.p.rapidapi.com/ip/check?format=json';
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '4a3835d161msha2d965d4ca4cc6bp14c0c0jsn17855714a692',
-        'X-RapidAPI-Host': 'ip-geo-location.p.rapidapi.com'
-    }
-};
-
-try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-} catch (error) {
-    console.error(error);
-}
+if ("geolocation" in navigator) {
+    /* местоположение доступно */
+    console.log(navigator.getCurrentPosition)
+} else {
+    /* местоположение НЕ доступно */
+    console.log('false')
 }
