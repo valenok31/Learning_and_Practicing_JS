9812b97f228d0f82075080f2cd75a3ba08f04379
Andrey <priv1984@yandex.ru>
2023-07-07 00:45:34 +0700

    checkResult

--- 001.js
+++ 001.js
@@ -1,4 +1,7 @@
 function lalu (numberCard){
+    //let arrNumberCard = numberCard.split('');
+    let arrNumberCard = Array.from(String(numberCard), Number);
+    console.log(arrNumberCard)
     let checkResult = true;
     return checkResult
 }
