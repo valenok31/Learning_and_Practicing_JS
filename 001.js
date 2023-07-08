720ef3d92ee37af692ca16cdb871191a56c6c162
Andrey <priv1984@yandex.ru>
2023-07-08 23:15:29 +0700

    checkResult

--- 001.js
+++ 001.js
@@ -1,9 +1,16 @@
-function lalu (numberCard){
-    //let arrNumberCard = numberCard.split('');
+function lalu(numberCard) {
     let arrNumberCard = Array.from(String(numberCard), Number);
-    console.log(arrNumberCard)
-    let checkResult = true;
-    return checkResult
+    let checkResult = arrNumberCard.map((n, x) => {
+            if (!(x % 2)) {
+                if (n * 2 < 10) {
+                    return n * 2
+                }
+                return (n * 2 - 10) + 1
+            }
+            return n
+        }
+    )
+    return !(checkResult.reduce((a, b) => a + b) % 10)
 }
 
-console.log(lalu(345234))
+console.log(lalu(4586_6549_4568_4585))
