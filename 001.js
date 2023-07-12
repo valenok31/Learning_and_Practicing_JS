3011f8c97359faebd94a502dbd88716cf345cd51
Andrey <priv1984@yandex.ru>
2023-07-12 23:12:35 +0700

    checkResult

--- 001.js
+++ 001.js
@@ -1,16 +1,26 @@
 function luhn(numberCard) {
-    if (Array.from(String(numberCard))[0]==5){
+    if (Array.from(String(numberCard))[0] == 5) {
         console.log('mir')
     }
-switch(Array.from(String(numberCard))[0]){
-    case '5': console.log('mir')
-}
+    switch (Array.from(String(numberCard))[0]) {
+        case '5':
+            console.log('mir')
+            break;
+        case '4':
+            console.log('visa')
+            break;
+        case '3':
+            console.log('master card')
+            break;
+        default:
+            console.log('null')
+    }
 
-        let x2 = (n, x) => {
-            if (!!(x % 2)) return n
-            if (n * 2 < 10) return n * 2
-            return (n * 2 - 10) + 1
-        }
+    let x2 = (n, x) => {
+        if (!!(x % 2)) return n
+        if (n * 2 < 10) return n * 2
+        return (n * 2 - 10) + 1
+    }
     return !(Array
         .from(String(numberCard), Number)
         .map((n, x) => x2(n, x))
