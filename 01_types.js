95f794817c0581a5aa8b64caf024948294b6da94
Andrey <priv1984@yandex.ru>
2023-07-24 23:11:59 +0700

    async

--- 01_types.js
+++ 01_types.js
@@ -1,8 +1,15 @@
 async function f() {
-    let a = await 4;
-    console.log(a)
+    let a = 4;
+    console.log('a1='+a)
+
+   a = setTimeout(()=>{
+        a = 20;
+        console.log('a2='+a)
+        return a
+    },1000)
+    console.log('a3='+a)
     return a;
 }
 
 
-f().then((r)=>console.log(r));
+f().then((r) => console.log('a4='+r));
