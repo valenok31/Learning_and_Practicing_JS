2dfdfa0259158f413edc1edb6e314f9c4e74f440
Andrey <priv1984@yandex.ru>
2023-07-25 23:44:12 +0700

    Prototype

--- 01_types.js
+++ 01_types.js
@@ -1,15 +1,8 @@
 async function f() {
-    let a = 4;
-    console.log('a1='+a)
 
-   a = setTimeout(()=>{
-        a = 20;
-        console.log('a2='+a)
-        return a
-    },1000)
-    console.log('a3='+a)
-    return a;
 }
 
 
-f().then((r) => console.log('a4='+r));
+let promis = new Promise(()=>{});
+
+promis.then(console.log)
