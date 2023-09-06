aa8679282af7dff0fc6cfd90f9f1b2a007289239
Andrey <priv1984@yandex.ru>
2023-09-07 00:14:24 +0700

    Javascript the Good Parts

--- 01_types.js
+++ 01_types.js
@@ -1,20 +1,4 @@
-try{
+let ret=0;
+let obj={ret};
 
-
-for(let i = 0; i<10; i++){
-    console.log(i)
-    if(i===4){
-throw 'ошибка выполнения. Начните с 5 шага'
-    }
-}
-
-}
-catch(e) {
-    console.log('error ' + e)
-}
-
-let ret=34;
-let rot='lkjs';
-let obj = {ret,rot:null}
-
-console.log(obj.rot)
+console.log(obj)
