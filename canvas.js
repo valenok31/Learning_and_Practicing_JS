59287858b1ac7497cb18d3c062b92788710bc600
Andrey <priv1984@yandex.ru>
2023-09-30 22:15:32 +0700

    teaching is light

--- canvas.js
+++ canvas.js
@@ -1,8 +1,25 @@
-let o=[];// = new Object();
- o.a = 42;
-o.b = 'fgs';
-o.c = true;
-o.push(5)
-//o = [...o,8,9,8,7]
-o.length=3
-console.log(o);
+async function showAvatar() {
+
+    // запрашиваем JSON с данными пользователя
+    let response = await fetch('https://learn.javascript.ru/article/promise-chaining/user.json');
+    let user = await response.json();
+
+    // запрашиваем информацию об этом пользователе из github
+    let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
+    let githubUser = await githubResponse.json();
+
+    // отображаем аватар пользователя
+    let img = document.createElement('img');
+    img.src = githubUser.avatar_url;
+    img.className = "promise-avatar-example";
+    document.body.append(img);
+
+    // ждём 3 секунды и затем скрываем аватар
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+
+    img.remove();
+
+    return githubUser;
+}
+
+showAvatar();
