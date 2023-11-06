0adf244419b7b38e2c7bafb4e7fb4de62ad3da6b
Andrey <priv1984@yandex.ru>
2023-11-06 20:49:12 +0700

    4 kyu Chinese Numeral Encoder

--- canvas.js
+++ canvas.js
@@ -1,10 +1,21 @@
-function func(){
-    var a = 0;
-    return function(){
-        return a++;
-    }
+function toChineseNumeral(num){
+    var numerals = {
+        "-":"负",
+        ".":"点",
+        0:"零",
+        1:"一",
+        2:"二",
+        3:"三",
+        4:"四",
+        5:"五",
+        6:"六",
+        7:"七",
+        8:"八",
+        9:"九",
+        10:"十",
+        100:"百",
+        1000:"千",
+        10000:"万"
+    };
+
 }
-
-
-
-console.log(func()());
