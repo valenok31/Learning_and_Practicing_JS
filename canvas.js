4b91450e0ca8ee6a38907e0329ac7aab12842c6c
Andrey <priv1984@yandex.ru>
2023-11-09 00:01:53 +0700

    4 kyu / solution Chinese Numeral Encoder

--- canvas.js
+++ canvas.js
@@ -17,14 +17,18 @@ function toChineseNumeral(num) {
         1000: "千",
         10000: "万"
     };
-    let but = String(num).split('.');
-    //console.log(but)
-    let arr_num = String(but[0]).split('');
-    if(but[0]==10){
-        arr_num = [but[0]];
+    let negativ = false;
+    if (num < 0) {
+        num = -num;
+        negativ = true;
     }
-    if(but[0]<20 && but[0]>10){
+    let but = String(num).split('.');
 
+
+    let arr_num = String(but[0]).split('');
+
+    if (but[0] == 10) {
+        arr_num = [but[0]];
     }
 
     let arr_dot = String(but[1]).split('');
@@ -42,18 +46,29 @@ function toChineseNumeral(num) {
             num_chin.push(numerals[10 ** (arr_num.length - i - 1)]);
         }
     }
-if(but.length>1){
-    for (let i = 0; i < arr_dot.length; i++) {
-        dot_chin.push(numerals[arr_dot[i]]);
+    if (but.length > 1) {
+        for (let i = 0; i < arr_dot.length; i++) {
+            dot_chin.push(numerals[arr_dot[i]]);
+        }
+        dot_chin.unshift("点")
     }
-    dot_chin.unshift("点")
-}
 
-if(num_chin[num_chin.length - 1] === "零"){
-    num_chin.length = num_chin.length - 1;
-}
-    if(num_chin.length === 0){
+    if (num_chin[num_chin.length - 1] === "零") {
+        num_chin.length = num_chin.length - 1;
+    }
+    if (num_chin.length === 0) {
         num_chin = ["零"];
+    }
+    if(but[0]<20 && but[0]>10){
+        num_chin.length=0;
+        num_chin.push("十");
+        num_chin.push(numerals[but[0]-10]);
+    }
+
+
+
+    if (negativ) {
+        num_chin.unshift("负");
     }
 
     return num_chin.join('').concat(dot_chin.join(''));
@@ -61,4 +76,4 @@ if(num_chin[num_chin.length - 1] === "零"){
 
 }
 
-console.log(toChineseNumeral(18))
+console.log(toChineseNumeral(54))
