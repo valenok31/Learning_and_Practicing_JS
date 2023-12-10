d06e239f9b07810a48b428a551f7b64c73f9b445
Andrey <priv1984@yandex.ru>
2023-12-10 17:45:48 +0700

    Coordinate Generator

--- reactivity.js
+++ reactivity.js
@@ -1,49 +1,23 @@
-const data = {
-    "buyingPrice" : 200,
-    "sellingPrice": 400
-}
-let target = null;
-class DependancyTracker{
-    constructor(){
-        this.subscribers = []
+// Долгота -180 до +180
+function generateRandomLong() {
+    var num = (Math.random() * 180).toFixed(3);
+    var posorneg = Math.random();
+    if (posorneg > 0.5) {
+        num = num * -1;
     }
-    depend(){
-        if(target && this.subscribers.includes(target) !== true){
-            this.subscribers.push(target);
-        }
-    }
-    notify(){
-        for (let i = 0; i < this.subscribers.length; i++) {
-            this.subscribers[i]();
-        }
-    }
+
+    return Math.round(num / 5) * 5;
 }
-Object.keys(data).forEach(key => {
-    let internal = data[key];
-    let dep = new DependancyTracker()
-    Object.defineProperty(data, key, {
-        get: function() {
-            dep.depend(); // link target function
-            return internal;
-        },
-        set: function(val) {
-            internal = val; // set value
-            dep.notify(); // notify dependent variables linked funtion
-        }
-    })
-})
-// Watch function
-function watch(func){
-    target = func;
-    target();
-    target = null;
+
+// Широта -90 до +90
+function generateRandomLat() {
+    var num = (Math.random() * 90).toFixed(3);
+    var posorneg = Math.random();
+    if (posorneg > 0.5) {
+        num = num * -1;
+    }
+    return Math.round(num / 5) * 5 ;
 }
-// Link calculateProfit function in watch
-watch(() => {
-    data.profit = data.sellingPrice - data.buyingPrice
-})
-console.log("Profit : "+data.profit) // Profit : 200
-data.sellingPrice = 700
-console.log("Profit : "+data.profit) // Profit : 500
-data.sellingPrice = 900
-console.log("Profit : "+data.profit) // Profit : 700
+
+
+console.log(generateRandomLat() + " / " + generateRandomLong())
